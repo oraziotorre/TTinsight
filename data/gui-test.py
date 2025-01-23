50767498aad269273d3cdeed40d814eeaf663100
FrancescoDurante10 <francescodurante03@gmail.com>
2025-01-23 23:53:16 +0100

    Added new features to the gui to support the models

--- data/gui-test.py
+++ data/gui-test.py
@@ -1,5 +1,7 @@
 import math
 import sys
+import joblib
+import numpy as np
 import matplotlib.pyplot as plt
 from PyQt5.QtWidgets import (
     QApplication, QWidget, QHBoxLayout, QVBoxLayout, QLabel, QRadioButton, QGroupBox, QPushButton
@@ -10,14 +12,18 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 class MainWindow(QWidget):
     is_going_back = False
+
     def __init__(self):
         super().__init__()
+
+        self.model_logreg = joblib.load('models/LogReg.pkl')  # Carica il modello di regressione logistica
+
+        # self.model_lstm = load_model('models/LSTMboolean+3.keras')  # Carica il modello LSTM
 
         # Imposta dimensioni della finestra
         self.probabilities = None
         self.setWindowTitle("Div con form e colori personalizzati")
         self.setGeometry(100, 100, 1400, 700)  # x, y, larghezza, altezza
-
 
         # Mostra la finestra a tutto schermo
         self.showFullScreen()
@@ -38,7 +44,6 @@ class MainWindow(QWidget):
             border-right: 2px solid black;
         """)
         left_layout = QVBoxLayout()
-        left_layout.setAlignment(Qt.AlignTop)  # Allinea il contenuto in alto
         self.add_form_container(left_layout)
         self.add_bottom_div(left_layout)
 
@@ -50,9 +55,8 @@ class MainWindow(QWidget):
             background-color: #2a2f33;  /* Sfondo grigio scuro */
         """)
         right_layout = QVBoxLayout()
-        self.add_probability_graph(right_layout)
+        self.add_right_div(right_layout)
         self.right_div.setLayout(right_layout)
-
 
         # Aggiungere i due "div" al layout principale
         main_layout.addWidget(self.left_div)
@@ -65,6 +69,23 @@ class MainWindow(QWidget):
         self.setLayout(main_layout)
 
         self.scores = [[0, 0]]
+        self.points_progression = []
+        self.log_reg_values = [0, 0]
+        self.comeback_count = 0
+        self.lstm_values = [0, 0, 0]
+
+    def predict(self):
+
+
+        logreg_input = np.array(self.points_progression + self.log_reg_values).reshape(1,-1)
+        logreg_prob = self.model_logreg.predict_proba(logreg_input)[0]
+        print(f"LogReg Probabilities: {logreg_prob}")
+
+        '''
+        lstm_input = np.array(self.lstm_values + self.points_progression).reshape(1, -1)
+        lstm_prob = self.model_lstm.predict(lstm_input)
+        print(f"LSTM Probabilities: {lstm_prob}")
+        '''
 
     def keyPressEvent(self, event):
         """Gestisce la pressione dei tasti"""
@@ -94,7 +115,6 @@ class MainWindow(QWidget):
         layout.addWidget(form_container)
 
     def add_form_sections(self, layout):
-
         final_set_group = QGroupBox("")
         final_set_layout = QVBoxLayout()
 
@@ -102,21 +122,27 @@ class MainWindow(QWidget):
         utente_group = QGroupBox("")
         utente_layout = QVBoxLayout()
         utente_layout.setSpacing(5)
-        utente_layout.addWidget(QLabel("Player1"))
-        utente_layout.addWidget(QRadioButton("True"))
-        utente_layout.addWidget(QRadioButton("False"))
+        utente_layout.addWidget(QLabel("Final_Set_A"))
+        self.utente_radio_true = QRadioButton("1")
+        self.utente_radio_false = QRadioButton("0")
+        utente_layout.addWidget(self.utente_radio_true)
+        utente_layout.addWidget(self.utente_radio_false)
         utente_group.setLayout(utente_layout)
         utente_group.setStyleSheet("color: white;")
+        self.utente_radio_true.toggled.connect(self.update_lstm_values)
 
         # Gruppo per Avversario
         avversario_group = QGroupBox("")
         avversario_layout = QVBoxLayout()
         avversario_layout.setSpacing(5)
-        avversario_layout.addWidget(QLabel("Player2"))
-        avversario_layout.addWidget(QRadioButton("True"))
-        avversario_layout.addWidget(QRadioButton("False"))
+        avversario_layout.addWidget(QLabel("Final_Set_B"))
+        self.avversario_radio_true = QRadioButton("1")
+        self.avversario_radio_false = QRadioButton("0")
+        avversario_layout.addWidget(self.avversario_radio_true)
+        avversario_layout.addWidget(self.avversario_radio_false)
         avversario_group.setLayout(avversario_layout)
         avversario_group.setStyleSheet("color: white;")
+        self.avversario_radio_true.toggled.connect(self.update_lstm_values)
 
         # Aggiungi i gruppi al layout finale
         final_set_layout.addWidget(utente_group)
@@ -131,12 +157,15 @@ class MainWindow(QWidget):
         is_final_layout = QVBoxLayout()
         is_final_layout.setSpacing(10)
         is_final_layout.addWidget(QLabel("Is_Final:"))
-        is_final_layout.addWidget(QRadioButton("True"))
-        is_final_layout.addWidget(QRadioButton("False"))
+        self.is_final_radio_true = QRadioButton("1")
+        self.is_final_radio_false = QRadioButton("0")
+        is_final_layout.addWidget(self.is_final_radio_true)
+        is_final_layout.addWidget(self.is_final_radio_false)
         is_final_layout.addWidget(QLabel(""))
 
         is_final_group.setLayout(is_final_layout)
         is_final_group.setStyleSheet("color: white;")
+        self.is_final_radio_true.toggled.connect(self.update_lstm_values)
 
         # Bottone per mostrare/nascondere tutto
         self.toggle_button = QPushButton("Mostra Tutto")
@@ -179,8 +208,16 @@ class MainWindow(QWidget):
         layout.addWidget(is_final_group)
         layout.addLayout(button_layout)
 
+    def update_lstm_values(self):
+        """Aggiorna i valori in lstm_values in base ai radio button selezionati"""
+        self.lstm_values[0] = 1 if self.is_final_radio_true.isChecked() else 0
+        self.lstm_values[1] = 1 if self.utente_radio_true.isChecked() else 0
+        self.lstm_values[2] = 1 if self.avversario_radio_true.isChecked() else 0
+
+        print(f"Updated lstm_values: {self.lstm_values}")
+
     def toggle_radio_buttons(self):
-        """Mostra o nasconde i radio button"""
+        """Mostra o nascondi i radio button"""
         if self.radio_groups[0].isVisible():
             for group in self.radio_groups:
                 group.setVisible(False)
@@ -359,6 +396,163 @@ class MainWindow(QWidget):
         graph_div.setLayout(graph_layout)
         layout.addWidget(graph_div)
 
+    def add_right_div(self, layout):
+        # Creazione di un layout orizzontale principale
+        main_layout = QHBoxLayout()
+
+        # Creazione del primo div verticale (sinistra)
+        left_div = QWidget()
+        left_div.setStyleSheet("""
+            background-color: #1a1f23;
+            border-radius: 5px;
+            margin-top: 60px;
+            margin-bottom: 10px;
+            padding: 10px;
+            border: 0px solid;
+        """)
+        left_layout = QVBoxLayout()
+        left_layout.setContentsMargins(0, 0, 0, 0)
+        left_layout.setSpacing(10)
+
+        # Titolo per le probabilità (a sinistra)
+        title_label_left = QLabel("Probabilità Matematica")
+        title_label_left.setAlignment(Qt.AlignCenter)
+        title_label_left.setStyleSheet("""
+            font-size: 18px;
+            color: #f0f0f0;
+            font-weight: bold;
+        """)
+
+        # Etichette delle probabilità (a sinistra)
+        current_prob_label_left = QLabel("Probabilità Corrente: 0.0%")
+        player1_prob_label_left = QLabel("Player 1 fa punto: 0.0%")
+        player2_prob_label_left = QLabel("Player 2 fa punto: 0.0%")
+
+        # Aggiungo il titolo e le label al layout del div a sinistra
+        left_layout.addWidget(title_label_left)
+        for label in [current_prob_label_left, player1_prob_label_left, player2_prob_label_left]:
+            label.setAlignment(Qt.AlignCenter)
+            label.setStyleSheet("""
+                font-size: 14px;
+                color: white;
+            """)
+            left_layout.addWidget(label)
+
+        left_div.setLayout(left_layout)
+
+        # Creazione del div centrale (già esistente)
+        text_div = QWidget()
+        text_div.setStyleSheet("""
+            background-color: #1a1f23;
+            border-radius: 5px;
+            margin-top: 60px;
+            margin-bottom: 10px;
+            padding: 10px;
+            border: 0px solid;
+        """)
+
+        # Layout verticale per il testo
+        text_layout = QVBoxLayout()
+        text_layout.setContentsMargins(0, 0, 0, 0)
+        text_layout.setSpacing(10)
+
+        # Titolo per le probabilità
+        title_label = QLabel("Probabilità Matematica")
+        title_label.setAlignment(Qt.AlignCenter)
+        title_label.setStyleSheet("""
+            font-size: 18px;
+            color: #f0f0f0;
+            font-weight: bold;
+        """)
+
+        # Etichette delle probabilità
+        self.current_prob_label = QLabel("Probabilità Corrente: 0.0%")
+        self.player1_prob_label = QLabel("Player 1 fa punto: 0.0%")
+        self.player2_prob_label = QLabel("Player 2 fa punto: 0.0%")
+
+        # Aggiungo il titolo e le label al layout verticale
+        text_layout.addWidget(title_label)
+        for label in [self.current_prob_label, self.player1_prob_label, self.player2_prob_label]:
+            label.setAlignment(Qt.AlignCenter)
+            label.setStyleSheet("""
+                font-size: 14px;
+                color: white;
+            """)
+            text_layout.addWidget(label)
+
+        text_div.setLayout(text_layout)
+
+        # Creazione del div destro (a destra)
+        right_div = QWidget()
+        right_div.setStyleSheet("""
+            background-color: #1a1f23;
+            border-radius: 5px;
+            margin-top: 60px;
+            margin-bottom: 10px;
+            padding: 10px;
+            border: 0px solid;
+        """)
+        right_layout = QVBoxLayout()
+        right_layout.setContentsMargins(0, 0, 0, 0)
+        right_layout.setSpacing(10)
+
+        # Titolo per le probabilità (a destra)
+        title_label_right = QLabel("Probabilità Matematica")
+        title_label_right.setAlignment(Qt.AlignCenter)
+        title_label_right.setStyleSheet("""
+            font-size: 18px;
+            color: #f0f0f0;
+            font-weight: bold;
+        """)
+
+        # Etichette delle probabilità (a destra)
+        current_prob_label_right = QLabel("Probabilità Corrente: 0.0%")
+        player1_prob_label_right = QLabel("Player 1 fa punto: 0.0%")
+        player2_prob_label_right = QLabel("Player 2 fa punto: 0.0%")
+
+        # Aggiungo il titolo e le label al layout del div a destra
+        right_layout.addWidget(title_label_right)
+        for label in [current_prob_label_right, player1_prob_label_right, player2_prob_label_right]:
+            label.setAlignment(Qt.AlignCenter)
+            label.setStyleSheet("""
+                font-size: 14px;
+                color: white;
+            """)
+            right_layout.addWidget(label)
+
+        right_div.setLayout(right_layout)
+
+        # Div inferiore - grafico (come nel codice originale)
+        lower_div = QWidget()
+        lower_div.setStyleSheet("""
+            background-color: #1a1f23;
+            border-radius: 5px;
+            margin: 10px;
+            padding: 10px;
+        """)
+        lower_div.setFixedHeight(500)  # Occupa circa il 55% dello spazio verticale
+
+        # Layout per il grafico
+        lower_layout = QVBoxLayout()
+        lower_layout.setContentsMargins(10, 10, 10, 10)
+        lower_layout.setSpacing(10)
+
+        # Aggiungere il grafico usando la funzione esistente
+        self.add_probability_graph(lower_layout)
+        lower_div.setLayout(lower_layout)
+
+        # Aggiungere i div al layout orizzontale principale
+        main_layout.addWidget(left_div)
+        main_layout.addWidget(text_div)
+        main_layout.addWidget(right_div)
+
+        # Aggiungere il div inferiore per il grafico
+        layout.addLayout(main_layout)
+        layout.addWidget(lower_div)
+
+        # Rimuovere lo spazio residuo per non occupare tutto lo spazio
+        layout.addStretch()
+
     def add_graph(self, layout):
         """Aggiunge un grafico sotto i numeri"""
 
@@ -445,7 +639,7 @@ class MainWindow(QWidget):
         # Aggiungi la percentuale sopra ogni pallino
         for i, (x, y) in enumerate(zip(x_values, y_values)):
             # Percentuale sopra ogni pallino
-            self.axes_probability.annotate(f"{y * 100:.4f}%", (x, y),
+            self.axes_probability.annotate(f"{y:.4f}%", (x, y),
                                            textcoords="offset points", xytext=(0, 10), ha='center', color="blue")
 
         # Calcola le probabilità per gli scenari futuri
@@ -483,10 +677,12 @@ class MainWindow(QWidget):
             self.axes_probability.scatter([future_x], [prob_scenario2], color="red", marker='o')
 
             # Aggiungi la percentuale sopra ogni pallino per gli scenari futuri
-            self.axes_probability.annotate(f"{prob_scenario1 * 100:.4f}%", (future_x, prob_scenario1),
+            self.axes_probability.annotate(f"{prob_scenario1:.4f}%", (future_x, prob_scenario1),
                                            textcoords="offset points", xytext=(0, 10), ha='center', color="green")
-            self.axes_probability.annotate(f"{prob_scenario2 * 100:.4f}%", (future_x, prob_scenario2),
+            self.axes_probability.annotate(f"{prob_scenario2:.4f}%", (future_x, prob_scenario2),
                                            textcoords="offset points", xytext=(0, 10), ha='center', color="red")
+            self.player1_prob_label.setText(f"Player 1 fa punto: {prob_scenario1:.8f}%")
+            self.player2_prob_label.setText(f"Player 2 fa punto: {prob_scenario2:.8f}%")
 
         total_points = value1 + value2
         half_visible = 8  # Range minimo visibile
@@ -510,6 +706,9 @@ class MainWindow(QWidget):
         self.axes_probability.set_ylabel("Probabilità")
         self.axes_probability.legend()
 
+        # Aggiorna le label con le probabilità calcolate
+        self.current_prob_label.setText(f"Probabilità Corrente: {prob_value:.8f}%")
+
         # Aggiorna il canvas
         self.canvas_probability.draw()
 
@@ -528,53 +727,101 @@ class MainWindow(QWidget):
         total = sum_part1 + part2
         return total
 
+    def update_comeback_and_length(self):
+        """Aggiorna il comeback e la lunghezza in base a points_progression"""
+        print(f"Updating comeback and length. Current points_progression: {self.points_progression}")
+
+        self.log_reg_values[1] = len(self.points_progression)
+        print(f"Updated length (log_reg_values[1]): {self.log_reg_values[1]}")
+
+        if self.log_reg_values[1] == 1:
+            if self.points_progression[-1] == 0:
+                self.log_reg_values[0] = -1
+            else:
+                self.log_reg_values[0] = 1
+        else:
+            # Controlliamo il valore corrente di comeback
+            last_value = self.points_progression[-1]
+            secondtolast_value = self.points_progression[-2]
+
+            if last_value == 1:
+                if last_value == secondtolast_value:
+                    self.log_reg_values[0] += 1
+                else:
+                    self.log_reg_values[0] = 1
+            elif last_value == 0:
+                if last_value == secondtolast_value:
+                    self.log_reg_values[0] -= 1
+                else:
+                    self.log_reg_values[0] = -1
+
+        print(f"Updated comeback (log_reg_values[0]): {self.log_reg_values[0]}")
+
     def increment_label1(self):
         """Incrementa il valore del primo numero"""
         value1 = int(self.label1.text())
         value2 = int(self.label2.text())
+        print(f"Incrementing label1. Current values: label1 = {value1}, label2 = {value2}")
+
         if value1 < 11 and value1 + value2 < 20 and value2 < 11:
             self.label1.setText(str(value1 + 1))
             self.scores.append([value1 + 1, value2])
+            self.points_progression.append(1)  # Aggiungi 1 per incrementare il comeback
+            print(f"Appended 1 to points_progression: {self.points_progression}")
+            self.update_comeback_and_length()
             self.update_graph()
             self.update_probability_graph()
 
-
-
     def increment_label2(self):
-
+        """Incrementa il valore del secondo numero"""
         value1 = int(self.label1.text())
         value2 = int(self.label2.text())
+        print(f"Incrementing label2. Current values: label1 = {value1}, label2 = {value2}")
+
         if value2 < 11 and value1 + value2 < 20 and value1 < 11:
             self.label2.setText(str(value2 + 1))
             self.scores.append([value1, value2 + 1])
+            self.points_progression.append(0)  # Aggiungi 0 per diminuire il comeback
+            print(f"Appended 0 to points_progression: {self.points_progression}")
+            self.update_comeback_and_length()
             self.update_graph()
             self.update_probability_graph()
 
     def reset_labels(self):
         """Resetta entrambi i valori a zero"""
+        print("Resetting labels...")
         self.label1.setText("0")
         self.label2.setText("0")
-        self.probabilities = []  # Vuota la lista
+        self.probabilities = []
+        self.points_progression = []
+        self.log_reg_values = [0, 0]  # Reset both comeback and length
         self.scores = [[0, 0]]
+        print(f"points_progression reset: {self.points_progression}")
         self.update_graph()
         self.update_probability_graph()
 
     def go_back(self):
         """Torna indietro di un passo nei punteggi"""
+        print("Going back one step...")
         if len(self.scores) > 1:
             self.scores.pop()
+            self.points_progression.pop()
+            print(f"Removed last score. Current scores: {self.scores}")
+            print(f"Removed last point from points_progression. Current points_progression: {self.points_progression}")
+            self.update_comeback_and_length()
             last_score = self.scores[-1]
             self.label1.setText(str(last_score[0]))
             self.label2.setText(str(last_score[1]))
-            # Pulizia del grafico prima di ridisegnarlo
             self.axes_probability.clear()
             self.is_going_back = True
             self.update_graph()
             self.update_probability_graph()
 
 
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     main_window = MainWindow()
     main_window.show()
+    logreg_prob, lstm_prob = main_window.predict()
     sys.exit(app.exec_())
